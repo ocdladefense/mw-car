adcd693675fe17aa1cdd93cf852bdad5a15761d5
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-01-26 11:28:52 -0800

    Only show the 'by author' and publish date if the fields have a valid value.  If court is not set for a set of reviews, show 'Case Reviews' in the header.

--- templates/summary.tpl.php
+++ templates/summary.tpl.php
@@ -9,7 +9,16 @@
         <a href="<?php print "$appDomain/car/list?year=$year&month=$month&day=$day&court=$court"; ?>"><?php print $title; ?></a>
     </h2>
     <div class="car-summary-header">
-        <p>by: <?php print $author . " &#8226; " . $publishDate?></p>
+        <p>
+            <?php
+                $summaryHeader = "";
+                if(!empty($author)) $summaryHeader .= "by: $author";
+                if(!empty($author) && !empty($publishDate)) $summaryHeader .= " &#8226; ";
+                if(!empty($publishDate)) $summaryHeader .= "$publishDate";
+
+                print $summaryHeader;
+            ?>
+        </p>
     </div>
 
     <?php print $subjectsHTML; ?>
