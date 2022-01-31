d8192b6f9702e22e8edbcf27dc1ebe535d604dd9
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2022-01-31 02:32:18 +0000

    Summary links should be vertical.

--- templates/summary-links.tpl.php
+++ templates/summary-links.tpl.php
@@ -1,8 +1,10 @@
 <h1>OCDLA Library of Defense - Case Reviews</h1>
 
 
-<?php foreach($allSummaryLinks as $year => $url) : ?>
-
-    <a href="<?php print $url; ?>"><?php print $year; ?> Case Summaries by Topic</a></br>
-
-<?php endforeach; ?>
+<ul>
+    <?php foreach($allSummaryLinks as $year => $url) : ?>
+        <li class="summary-link">
+            <a href="<?php print $url; ?>"><?php print $year; ?> Case Summaries by Topic</a>
+        </li>
+    <?php endforeach; ?>
+</ul>
