9184bc1314493c361bc9bcb5cfc39759dfa3455d
Ubuntu <ubuntu@ip-172-31-22-227.us-west-2.compute.internal>
2022-01-31 03:29:39 +0000

    Template cleanup.

--- templates/summary.tpl.php
+++ templates/summary.tpl.php
@@ -6,8 +6,11 @@
 <div class="car-entry">
 
     <h2>
-        <a href="<?php print "$appDomain/car/list?year=$year&month=$month&day=$day&court=$court"; ?>"><?php print $title; ?></a>
+        <a href="<?php print "$appDomain/car/list?year=$year&month=$month&day=$day&court=$court"; ?>">
+            <?php print $title; ?>
+        </a>
     </h2>
+
     <div class="car-summary-header">
         <p>
             <?php
@@ -23,6 +26,10 @@
 
     <?php print $subjectsHTML; ?>
     
-    <p><a href="<?php print "$appDomain/car/list?year=$year&month=$month&day=$day&court=$court"; ?>">&rarr; read the full summaries...</a></p>
+    <p>
+        <a href="<?php print "$appDomain/car/list?year=$year&month=$month&day=$day&court=$court"; ?>">
+            &rarr; read the full summaries...
+        </a>
+    </p>
 
 </div>
